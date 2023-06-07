24bb85f534a374e21b8251a7d96e0debb7ca1bdd
GEL2345 <G.E.Loake@lse.ac.uk>
2023-06-07 11:42:30 +0100

    Primitive Types work

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        int age = 30;
-        age = 35;
-        System.out.println(age);
+       byte age = 30;
+       long viewsCount = 3_123_456_789L;
+       float price = 10.99F;
+       char letter = 'A';
+       boolean isEligible = false;
 
     }
 }
