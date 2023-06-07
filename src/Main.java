abfb6469e1414934d191306f24496469343d17a3
GEL2345 <G.E.Loake@lse.ac.uk>
2023-06-07 16:23:27 +0100

    The Math Class

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,16 @@
 
 public class Main {
     public static void main(String[] args) {
-//    implicit Casting
-//      byte > short > int > long > float > double
-//        double x = 1.1;
-////        double y = x + 2;
-//        double y = (int)x + 2; //Explicit casting between compatible types
-//        System.out.println(y);
-//        String x = "1";
-//        int y = Integer.parseInt(x) + 2;
-//        System.out.println(y);
+    int result = (int) Math.ceil(1.1F);
+    System.out.println(result);
+    int result2 = (int) Math.floor(1.1F);
+    System.out.println(result2);
+    int max = Math.max(1,2);
+    System.out.println(max);
+    int min = Math.max(1,2);
+    System.out.println(min);
+    int rand = (int) Math.round(Math.random()* 100);
+    int roundrand = (int) (Math.random() * 100);
+    System.out.println(roundrand);
     }
 }
