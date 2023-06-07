7b986b14fd114b263dd0fb846abb2b5083b4a56a
GEL2345 <G.E.Loake@lse.ac.uk>
2023-06-07 14:10:48 +0100

    Arrays

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,17 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        String message = "Hello \"Mosh\"";
-        System.out.println(message);
-        String message1 = "C:\\Windows\\...";
-        System.out.println(message1);
-        String message2 = "C:\nWindows\\...";
-        System.out.println(message2);
-        String message3 = "C:\tWindows\\...";
-        System.out.println(message3);
-
-
+//        int[] numbers = new int[5];
+//        numbers[0] = 1;
+//        numbers[1] = 2;
+//        numbers[10] = 3; Gives Exception
+//        System.out.println(numbers); //Gives memory address
+//        System.out.println(Arrays.toString(numbers));
+        int[] numbers = {2, 3, 5, 1, 4};
+        System.out.println(numbers.length);
+        System.out.println(Arrays.toString(numbers));
+        Arrays.sort(numbers);
+        System.out.println(Arrays.toString(numbers));
     }
 }
