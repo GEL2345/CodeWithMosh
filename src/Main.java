8704f72e1f3bbe850e38e4d15e46db69f8eff3c5
GEL2345 <G.E.Loake@lse.ac.uk>
2023-06-07 13:33:46 +0100

    Strings

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
-import java.awt.*;
-
 public class Main {
     public static void main(String[] args) {
-        Point point1 = new Point(1,1);
-        Point point2 = point1;
-        point1.x = 2;
-        System.out.println(point2);
-
+//        String message = new String("Hello World");
+        String message = "  Hello World" + "!!";
+        System.out.println(message);
+        System.out.println(message.endsWith("!!"));
+        System.out.println(message.startsWith("!!"));
+        System.out.println(message.length());
+        System.out.println(message.indexOf("e"));
+        System.out.println(message.replace("World", "People"));
+        System.out.println(message.toLowerCase());
+        System.out.println(message.toUpperCase());
+        System.out.println(message.trim());
     }
 }
