82926995a9612345816ecd476481d91c3bf57309
GEL2345 <G.E.Loake@lse.ac.uk>
2023-06-07 14:23:21 +0100

    Multi-Dimensional Arrays

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,10 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-//        int[] numbers = new int[5];
-//        numbers[0] = 1;
-//        numbers[1] = 2;
-//        numbers[10] = 3; Gives Exception
-//        System.out.println(numbers); //Gives memory address
-//        System.out.println(Arrays.toString(numbers));
-        int[] numbers = {2, 3, 5, 1, 4};
-        System.out.println(numbers.length);
-        System.out.println(Arrays.toString(numbers));
-        Arrays.sort(numbers);
-        System.out.println(Arrays.toString(numbers));
+//        int [] [] numbers = new int[2][3];
+//        numbers[0][0] = 1;
+//        System.out.println(Arrays.deepToString(numbers));
+        int [][] numbers = {{1,2,3},{4,5,6}};
+        System.out.println(Arrays.deepToString(numbers));
     }
 }
