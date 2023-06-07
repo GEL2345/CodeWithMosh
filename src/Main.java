1084d6277a4c657cd87823c910032a77aceb1006
GEL2345 <G.E.Loake@lse.ac.uk>
2023-06-07 13:49:20 +0100

    Escape Sequences

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-//        String message = new String("Hello World");
-        String message = "  Hello World" + "!!";
+        String message = "Hello \"Mosh\"";
         System.out.println(message);
-        System.out.println(message.endsWith("!!"));
-        System.out.println(message.startsWith("!!"));
-        System.out.println(message.length());
-        System.out.println(message.indexOf("e"));
-        System.out.println(message.replace("World", "People"));
-        System.out.println(message.toLowerCase());
-        System.out.println(message.toUpperCase());
-        System.out.println(message.trim());
+        String message1 = "C:\\Windows\\...";
+        System.out.println(message1);
+        String message2 = "C:\nWindows\\...";
+        System.out.println(message2);
+        String message3 = "C:\tWindows\\...";
+        System.out.println(message3);
+
+
     }
 }
