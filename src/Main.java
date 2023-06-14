6a59a6c50ff4ee3e02eb408c10fee7467577154b
GEL2345 <G.E.Loake@lse.ac.uk>
2023-06-14 13:48:48 +0100

    For Loops

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,8 @@
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String input = "";
-        Scanner scanner = new Scanner(System.in);
-
-        while (!input.equals("quit")) {
-            System.out.println("Input: ");
-            input = scanner.next().toLowerCase();
-            System.out.println(input);
-
+       for (int i = 0; i < 5; i++)
+           System.out.println("Hello world " + i);
         }
     }
-}
+
