796540930f7a4eb66628008115c3138299f28c04
GEL2345 <G.E.Loake@lse.ac.uk>
2023-06-14 11:01:48 +0100

    Comparison Operators

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,15 @@
-import java.text.NumberFormat;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-    final byte MONTHS_IN_YEAR = 12;
-    final byte PERCENT = 100;
+  int x = 1;
+  int y = 1;
 
-    Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
-
-        System.out.print("Annual Interest Rate: ");
-        float annualInterest = scanner.nextFloat();
-        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
-
-        System.out.print("Period (Years): ");
-        byte years = scanner.nextByte();
-        int numberOfPayments = years * MONTHS_IN_YEAR;
-
-        double mortgage = principal
-                * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
-                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
-
-        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
-        System.out.println("Mortgage: " + mortgageFormatted);
+        System.out.println(x == y); // Equal to
+        System.out.println(x != y); // Not Equal to
+        System.out.println(x > y); // Greater than
+        System.out.println(x >= y); // Greater than OR Equal to
+        System.out.println(x < y); // Less than
+        System.out.println(x <= y); // Greater than OR Equal to
 
     }
 }
