7ad1739c7360c1a7c0ccdadbfd5311ed1e617a5a
GEL2345 <G.E.Loake@lse.ac.uk>
2023-06-14 09:07:31 +0100

    Reading Input

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,13 @@
-import java.text.NumberFormat;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-//        NumberFormat currency = new NumberFormat() Abstract class
-      NumberFormat currency = NumberFormat.getCurrencyInstance();
-        String result = currency.format(1234567.891);
-        System.out.println(result);
-        NumberFormat percent = NumberFormat.getPercentInstance();
-        String PerResult = percent.format(0.1);
-        System.out.println(PerResult);
-
-        String res = NumberFormat.getPercentInstance().format(0.1);
-        System.out.println(res);
-
-
+        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Age: ");
+//        byte age = scanner.nextByte();
+        System.out.print("Name: ");
+//        String name = scanner.next();
+        String name = scanner.nextLine();
+        System.out.println("You are " + name);
     }
 }
