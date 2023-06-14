a043e0b65c57533fa6d7696a2a4e0c087c3b8758
GEL2345 <G.E.Loake@lse.ac.uk>
2023-06-14 13:26:35 +0100

    FizzBuzz Attempt - Succesful :)

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,22 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
- String role = "admin";
-    switch (role){
-        case "admin":
-            System.out.println("You're an admin");
-            break;
-        case "moderator":
-            System.out.println("You're a moderator");
-            break;
-        default:
-            System.out.println("You're a guest");
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int x = sc.nextInt();
+        // Input - 5 = Fizz, 3 = Buzz, 3 and 5 = FizzBuzz
+        if (x % 3 == 0 && x % 5 == 0)
+            System.out.println("FizzBuzz");
+        else if (x % 5 == 0) {
+            System.out.println("Fizz");}
+        else if (x % 3 == 0) {
+            System.out.println("Buzz");
+        }
+        else
+            System.out.println(x);
+
     }
-         if (role == "admin")
-            System.out.println("You're an Admin!");
-         else if (role == "moderator")
-             System.out.println("You're a Moderator!");
-         else
-             System.out.println("You're a guest");
-             
-         }
-}
+
+
+    }
