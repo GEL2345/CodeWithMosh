24a5913420a391b10ea785506999238d38cd697d
GEL2345 <G.E.Loake@lse.ac.uk>
2023-06-14 10:14:25 +0100

    Mortgage exercise attempt

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,28 @@
+import java.security.Principal;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+//        Principal = Amount being loaned to you - Deposit + Principal = Total Amount
+//        Annual Interest Rate (R) , Duration (Years)
+//        Mortgage = P x by (R(1 + R)N (Power of N)) / ((1 + R)N (Power of N) - 1)
+
+//        Ask for Principal (P)
         Scanner scanner = new Scanner(System.in);
-//        System.out.print("Age: ");
-//        byte age = scanner.nextByte();
-        System.out.print("Name: ");
-//        String name = scanner.next();
-        String name = scanner.nextLine();
-        System.out.println("You are " + name);
+        System.out.print("How much are you wishing to borrow?: ");
+        long principal = scanner.nextLong();
+//        Ask Annual Interest Rate (R)
+        System.out.print("What is the annual interest rate?: ");
+        float interest = scanner.nextFloat();
+//        Ask for Duration (N * 12 is how many months)
+        System.out.print("How many years will the mortgage be for?: ");
+        int duration = scanner.nextInt();
+        int duration * 12;
+
+//        Math.pow();
+//        P x by (R(1 + R)N (Power of N)) / ((1 + R)N (Power of N) - 1)
+//        Math.pow()
+//        Calculate
+
     }
 }
