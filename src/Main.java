43f4d6c8b60e989c0e175324fdfee76bdfad4c30
GEL2345 <G.E.Loake@lse.ac.uk>
2023-06-14 11:30:07 +0100

    if Statements

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 
 public class Main {
     public static void main(String[] args) {
-//    int temperature = 12;
-//    boolean isWarm = temperature > 20 && temperature < 30;
-//        System.out.println(isWarm);
-
-        boolean hasHighIncome = true;
-        boolean hasGoodCredit = true;
-        boolean hasCriminalRecord = false;
-        boolean isEligible = (hasHighIncome || hasGoodCredit) && !hasCriminalRecord;
-
+        int temp = 32;
+        if (temp > 30) {
+            System.out.println("It's a hot day");
+            System.out.println("Drink Water");
+        }
+            else if (temp > 20 && temp <= 30)
+                System.out.println("Beautiful day");
+        else
+            System.out.println("Cold day");
     }
 }
