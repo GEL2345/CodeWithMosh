47c5152c83386ad579c9dff000298a0312cdcb46
GEL2345 <G.E.Loake@lse.ac.uk>
2023-06-14 13:47:12 +0100

    While Loops

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int x = sc.nextInt();
-        // Input - 5 = Fizz, 3 = Buzz, 3 and 5 = FizzBuzz
-        if (x % 3 == 0 && x % 5 == 0)
-            System.out.println("FizzBuzz");
-        else if (x % 5 == 0) {
-            System.out.println("Fizz");}
-        else if (x % 3 == 0) {
-            System.out.println("Buzz");
+        String input = "";
+        Scanner scanner = new Scanner(System.in);
+
+        while (!input.equals("quit")) {
+            System.out.println("Input: ");
+            input = scanner.next().toLowerCase();
+            System.out.println(input);
+
         }
-        else
-            System.out.println(x);
-
     }
-
-
-    }
+}
