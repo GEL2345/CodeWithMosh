73e45dd81294f65304e97e7e1a348cb3f77a5d5c
GEL2345 <G.E.Loake@lse.ac.uk>
2023-06-14 08:31:15 +0100

    Formatting Numbers to Percent and Currency

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,18 @@
+import java.text.NumberFormat;
 
 public class Main {
     public static void main(String[] args) {
-    int result = (int) Math.ceil(1.1F);
-    System.out.println(result);
-    int result2 = (int) Math.floor(1.1F);
-    System.out.println(result2);
-    int max = Math.max(1,2);
-    System.out.println(max);
-    int min = Math.max(1,2);
-    System.out.println(min);
-    int rand = (int) Math.round(Math.random()* 100);
-    int roundrand = (int) (Math.random() * 100);
-    System.out.println(roundrand);
+//        NumberFormat currency = new NumberFormat() Abstract class
+      NumberFormat currency = NumberFormat.getCurrencyInstance();
+        String result = currency.format(1234567.891);
+        System.out.println(result);
+        NumberFormat percent = NumberFormat.getPercentInstance();
+        String PerResult = percent.format(0.1);
+        System.out.println(PerResult);
+
+        String res = NumberFormat.getPercentInstance().format(0.1);
+        System.out.println(res);
+
+
     }
 }
