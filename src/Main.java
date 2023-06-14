b419d617da4b39fd18dc88eaaa70eb4caa79156f
GEL2345 <G.E.Loake@lse.ac.uk>
2023-06-14 11:53:17 +0100

    Ternary Operator

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,15 @@
 public class Main {
     public static void main(String[] args) {
         int income = 120_000;
-        boolean hasHighIncome = (income > 100_000);
+        String classname = income > 100_000 ? "First" : "Economy";
 
 //        if (income > 100_000)
-//            hasHighIncome = true;
+//            classname = "First";
 
 //        if (income > 100_000)
-//            hasHighIncome = true;
+//            classname = "First";
 //        else
-//            hasHighIncome = false;
-
+//            classname = "Economy";
 
     }
 }
